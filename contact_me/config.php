87e9891d61bbbf6c2ffa450f0f0deda63708c454
Roma Bilibov <romabilibov@gmail.com>
2019-12-01 14:36:58 +0100

    Config the email 'to' parameter

--- contact_me/config.php
+++ contact_me/config.php
@@ -3,8 +3,8 @@
 $email_config = [
   'from' => 'contact@valeriasafonova.com',
   'from_name' => 'My Website',
-  'to' => 'romabilibov@gmail.com',
-  'to_name' => 'Roma Bilibov',
+  'to' => 'Lera.safonova9292@gmail.com',
+  'to_name' => 'Lera Safonova',
   'subject' => 'Contact Form',
   'smtp' => [
     'host' => 'valeriasafonova.com',
