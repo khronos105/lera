0067b5142a7c617b66b5a04005852fe901c9a577
Roma Bilibov <romabilibov@gmail.com>
2019-12-01 14:27:43 +0100

    Added sending email from backend via SMTP

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Component({
@@ -13,23 +13,23 @@ export class ContactComponent implements OnInit {
    * Building form group and controllers
    */
   form = new FormGroup({
-    name: new FormControl('Roma Bilibov', [
+    name: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(30)
     ]),
-    email: new FormControl('romabilibov@gmail.com', [
+    email: new FormControl('', [
       Validators.email,
       Validators.required,
       Validators.minLength(8),
       Validators.maxLength(50)
     ]),
-    subject: new FormControl('Subject test', [
+    subject: new FormControl('', [
       Validators.required,
       Validators.minLength(5),
       Validators.maxLength(20)
     ]),
-    message: new FormControl('Message test', [
+    message: new FormControl('', [
       Validators.required,
       Validators.minLength(10),
       Validators.maxLength(450)
@@ -56,7 +56,19 @@ export class ContactComponent implements OnInit {
   }
 
   // url where the post will be sent
-  postUrl = 'http://lera.bilibov.com/contact/';
+  postUrl = 'http://lera.bilibov.com/contact_me/index.php';
+  //postUrl = 'http://localhost/lera/contact/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  // Loading animation
+  loading = false;
+  // Email status
+  emailStatus = null;
 
   /**
    * Constructor
@@ -67,17 +79,22 @@ export class ContactComponent implements OnInit {
   }
 
   sendEmail() {
+    this.loading = true;
     const contactMessage = {
       name: this.name.value,
       email: this.email.value,
       subject: this.subject.value,
       message: this.message.value,
     };
-    console.log(JSON.stringify(contactMessage));
-    this.http.post(this.postUrl, JSON.stringify(contactMessage))
+    this.http.post(this.postUrl, JSON.stringify({data: contactMessage}), this.httpOptions)
       .subscribe(
         response => {
-          console.log(response);
+          this.loading = false;
+          if (response === 1) {
+            this.emailStatus = true;
+          } else {
+            this.emailStatus = false;
+          }
         }
       )
     ;
